9d37f28cf918db5e8b01753f6183c5b08678cf80
anandkmr110043 <anand.tiwari@remedoapp.com>
2021-08-20 05:41:06 +0530

    WIP

--- src/com/company/DynamicArray.java
+++ src/com/company/DynamicArray.java
@@ -9478,7 +9478,7 @@ class SolutionHasPath {
     }
 
     // sliding window technique
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public int findMaxConsecutiveOneSliding(int[] nums) {
         int n = nums.length;
         int left = 0, right = 0, longest = 0, zeros = 0;
 
@@ -9501,5 +9501,80 @@ class SolutionHasPath {
         return longest;
     }
 
+    // cheer up solution for placing operators b/w numbers
+    public ArrayList<String> answer;
+    public String digits;
+    public long target;
 
+    public void recurse(int index, long previousOperand, long currentOperand, long value, ArrayList<String> ops) {
+        String nums = this.digits;
+
+        // Done processing all the digits in num
+        if (index == nums.length()) {
+
+            // If the final value == target expected AND
+            // no operand is left unprocessed
+            if (value == this.target && currentOperand == 0) {
+                StringBuilder sb = new StringBuilder();
+                ops.subList(1, ops.size()).forEach(v -> sb.append(v));
+                this.answer.add(sb.toString());
+            }
+            return;
+        }
+
+        // Extending the current operand by one digit
+        currentOperand = currentOperand * 10 + Character.getNumericValue(nums.charAt(index));
+        String current_val_rep = Long.toString(currentOperand);
+        int length = nums.length();
+
+        // To avoid cases where we have 1 + 05 or 1 * 05 since 05 won't be a
+        // valid operand. Hence this check
+        if (currentOperand > 0) {
+
+            // NO OP recursion
+            recurse(index + 1, previousOperand, currentOperand, value, ops);
+        }
+
+        // ADDITION
+        ops.add("+");
+        ops.add(current_val_rep);
+        recurse(index + 1, currentOperand, 0, value + currentOperand, ops);
+        ops.remove(ops.size() - 1);
+        ops.remove(ops.size() - 1);
+
+        if (ops.size() > 0) {
+
+            // SUBTRACTION
+            ops.add("-");
+            ops.add(current_val_rep);
+            recurse(index + 1, -currentOperand, 0, value - currentOperand, ops);
+            ops.remove(ops.size() - 1);
+            ops.remove(ops.size() - 1);
+
+            // MULTIPLICATION
+            ops.add("*");
+            ops.add(current_val_rep);
+            recurse(
+                    index + 1,
+                    currentOperand * previousOperand,
+                    0,
+                    value - previousOperand + (currentOperand * previousOperand),
+                    ops);
+            ops.remove(ops.size() - 1);
+            ops.remove(ops.size() - 1);
+        }
+    }
+
+    public List<String> addOperators(String num, int target) {
+
+        if (num.length() == 0) {
+            return new ArrayList<String>();
+        }
+
+        this.target = target;
+        this.digits = num;
+        this.answer = new ArrayList<String>();
+        this.recurse(0, 0, 0, 0, new ArrayList<String>());
+        return this.answer;
+    }
 }
--- src/com/company/EmployeeSolution.java
+++ src/com/company/EmployeeSolution.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class EmployeeSolution {
@@ -54,4 +56,118 @@ public class EmployeeSolution {
 
         return maxSum;
     }
+
+    Set<String> ans = new HashSet<>();
+
+    public List<String> addOperators(String num, int target) {
+        calculate(num, 0, target, "");
+        return new ArrayList<>(ans);
+    }
+
+    private void calculate(String num, int index, int target, String expression) {
+        if (index == num.length()) {
+
+
+            String res = expression.substring(0, expression.length() - 1);
+            int sum = 0;
+            try {
+                sum = Integer.parseInt(res);
+            } catch (Exception ex) {
+                if (!res.isEmpty()) sum = (int) Math.abs(calc(res));
+            }
+
+            if (sum == target) {
+                ans.add(res);
+            }
+        } else {
+
+            calculate(num, index + 1, target, expression + Integer.parseInt(String.valueOf(num.charAt(index))) + "*");
+            calculate(num, index + 1, target, expression + Integer.parseInt(String.valueOf(num.charAt(index))) + "+");
+            calculate(num, index + 1, target, expression + Integer.parseInt(String.valueOf(num.charAt(index))) + "-");
+            calculate(num, index + 1, target, expression + Integer.parseInt(String.valueOf(num.charAt(index))));
+
+        }
+    }
+
+    public Double calculate(String expression) {
+        if (expression == null || expression.length() == 0) {
+            return null;
+        }
+        return calc(expression.replace(" ", ""));
+    }
+
+    public Double calc(String expression) {
+
+        if (expression.startsWith("(") && expression.endsWith(")")) {
+            return calc(expression.substring(1, expression.length() - 1));
+        }
+        String[] containerArr = new String[]{expression};
+        double leftVal = getNextOperand(containerArr);
+        expression = containerArr[0];
+        if (expression.length() == 0) {
+            return leftVal;
+        }
+        char operator = expression.charAt(0);
+        expression = expression.substring(1);
+
+        while (operator == '*' || operator == '/') {
+            containerArr[0] = expression;
+            double rightVal = getNextOperand(containerArr);
+            expression = containerArr[0];
+            if (operator == '*') {
+                leftVal = leftVal * rightVal;
+            } else {
+                leftVal = leftVal / rightVal;
+            }
+            if (expression.length() > 0) {
+                operator = expression.charAt(0);
+                expression = expression.substring(1);
+            } else {
+                return leftVal;
+            }
+        }
+        if (operator == '+') {
+            return leftVal + calc(expression);
+        } else {
+            return leftVal - calc(expression);
+        }
+
+    }
+
+    private double getNextOperand(String[] exp) {
+        double res;
+        if (exp[0].startsWith("(")) {
+            int open = 1;
+            int i = 1;
+            while (open != 0) {
+                if (exp[0].charAt(i) == '(') {
+                    open++;
+                } else if (exp[0].charAt(i) == ')') {
+                    open--;
+                }
+                i++;
+            }
+            res = calc(exp[0].substring(1, i - 1));
+            exp[0] = exp[0].substring(i);
+        } else {
+            int i = 1;
+            if (exp[0].charAt(0) == '-') {
+                i++;
+            }
+            while (exp[0].length() > i && isNumber((int) exp[0].charAt(i))) {
+                i++;
+            }
+            res = Double.parseDouble(exp[0].substring(0, i));
+            exp[0] = exp[0].substring(i);
+        }
+        return res;
+    }
+
+
+    private boolean isNumber(int c) {
+        int zero = (int) '0';
+        int nine = (int) '9';
+        return (c >= zero && c <= nine) || c == '.';
+    }
+        
 }
