dbc2b007c082879eec999aa02d4c7cfac302d411
anatiwar <anatiwar@ciena.com>
2021-12-16 13:21:32 +0530

    CF practise started on 16/12/2021

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,71 @@
 package com.company;
 
-public class Main {
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class Main {
+    /*These are one of the fast methods of reading input and writing output*/
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out, true);
+        {
+            try {
+                int t = Integer.parseInt(br.readLine());
+                while (t-- > 0) {
+                    String firstLine = br.readLine();
+                    int n = Integer.parseInt(firstLine.split(" ")[0]);
+                    int k = Integer.parseInt(firstLine.split(" ")[1]);
+
+
+
+                    /*If you want to read numbers on the same line use StringTokenizer*/
+                    StringTokenizer st = new StringTokenizer(br.readLine());
+                    Integer[] arr = new Integer[n];
+
+                    for (int i = 0; i < n; ++i) {
+                        arr[i] = Integer.parseInt(st.nextToken());
+                    }
+
+
+                    // function to execute logic
+                    minimumPossibleScore(arr, 0, k);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // Handle the case for duplicates in array
+    private static void minimumPossibleScore(Integer[] arr, int score, int k) {
+        // Math.floor(ai/aj) to your score, where ⌊xy⌋ is the maximum integer not exceeding xy.
+        Arrays.sort(arr);
+        List<Integer> arrlist = new ArrayList<Integer>();
+
+        Collections.addAll(arrlist, arr);
+        while (arrlist.size() > 0 && k -- > 0) {
+            int num = arrlist.get(0);
+            int den = arrlist.get(arrlist.size() - 1);
+            score += Math.floorDiv(num, den);
+            arrlist.remove(0);
+            arrlist.remove(arrlist.size() - 1);
+        }
+        // Add remaining elements of array as it is:-
+        for (Integer elem: arrlist){
+            score += elem;
+        }
+
+        System.out.println(score);
     }
 }
-
