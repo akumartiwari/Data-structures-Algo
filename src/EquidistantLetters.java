13c90db6a3f8f2ef65721a88df9d5123916de573
anatiwar <anatiwar@ciena.com>
2022-01-18 04:25:44 +0530

    CF

---
+++ src/EquidistantLetters.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class EquidistantLetters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String str = sc.next();
+            rearrange(str);
+        }
+    }
+
+    private static void rearrange(String str) {
+        // create a Map of letters and count
+        Map<Character, Integer> freq = new HashMap<>();
+        boolean isDuplicate = false;
+        for (Character c : str.toCharArray()) {
+            int newF = freq.getOrDefault(c, 0) + 1;
+            if (newF >= 2) isDuplicate = true;
+            freq.put(c, newF);
+        }
+        if (!isDuplicate) {
+            System.out.println(str);
+        } else {
+            StringBuilder sb = new StringBuilder("");
+            HashMap<Character, Integer> nm = sortByValue(freq);
+            for (Map.Entry entry : nm.entrySet()) {
+                int times = (int) entry.getValue();
+                while (times-- > 0) sb.append(entry.getKey());
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+
+    // function to sort hashmap by values
+    public static HashMap<Character, Integer> sortByValue(Map<Character, Integer> hm) {
+        HashMap<Character, Integer> temp
+                = hm.entrySet()
+                .stream()
+                .sorted((i1, i2) -> i2.getValue().compareTo(i1.getValue()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new));
+
+        return temp;
+    }
+}
--- src/com/company/EmployeeSolution.java
+++ src/com/company/EmployeeSolution.java
@@ -1323,31 +1323,56 @@ Note that a period with one day is a smooth descent period by the definition.
 
     // TC = O(MLogM)
     // Author : Anand
+
+
+    /*
+    Input: n = 2, batteries = [3,3,3]
+    Output: 4
+    Explanation:
+    Initially, insert battery 0 into the first computer and battery 1 into the second computer.
+    After two minutes, remove battery 1 from the second computer and insert battery 2 instead. Note that battery 1 can still run for one minute.
+    At the end of the third minute, battery 0 is drained, and you need to remove it from the first computer and insert battery 1 instead.
+    By the end of the fourth minute, battery 1 is also drained, and the first computer is no longer running.
+    We can run the two computers simultaneously for at most 4 minutes, so we return 4.
+
+
+
+    2
+    [3,4,5]
+
+    sum = 3-1=2
+    [5,1_0000_00000_00000L]
+    level = 4 = 5 + 1  = 6
+    dist = 0 -> 1 -> 2
+    val = 5, nextVal = 1_0000_00000_00000L
+     */
     public long maxRunTime(int n, int[] batteries) {
-        Queue<Long> pq = new PriorityQueue<>();
-        for (long b : batteries)
-            pq.offer(b);
         long sum = 0;
-        while (pq.size() != n)
+        Queue<Long> pq = new PriorityQueue<>();
+        for (long b : batteries) pq.offer(b);
+        Arrays.sort(batteries);
+        while (pq.size() != n) {
             sum += pq.poll();
+        }
+        pq.offer(1_000000_0000_0000L);
 
-        pq.offer(1_00000_00000_00000L);  //10^14, redundant "great wall"
-
-        long dist = 0, level = pq.peek();
+        long dist = 0L, level = pq.peek();
         while (pq.size() != 1 && sum >= 0) {
             dist++;
-            long val = pq.poll(), nextVal = pq.peek();
-            if (nextVal > val) {
-                if ((nextVal - val) * dist <= sum) {
-                    sum -= (nextVal - val) * dist;
-                    level = nextVal;
-                } else {
-                    level += sum / dist;
-                    return level;
-                }
+            level = pq.poll();
+            long nextLevel = pq.peek();
+
+            if ((nextLevel - level) * dist <= sum) {
+                sum -= (nextLevel - level) * dist;
+                level = nextLevel;
+            } else {
+                level += sum / dist;
+                return level;
             }
         }
+
         return level;
+
     }
 }
 
