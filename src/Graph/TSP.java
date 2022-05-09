74c9d795ab5cd495ac254d1152720352327f20e5
anatiwar <anatiwar@ciena.com>
2022-05-09 19:58:58 +0530

    TSP based solution

--- src/Graph/TSP.java
+++ src/Graph/TSP.java
@@ -1,6 +1,8 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TSP {
     // It shows distance/wt of each vertices for start vertex
@@ -57,6 +59,57 @@ public class TSP {
         }
         return dp[mask][pos] = ans;
     }
+
+    // TC = O(n2*2^n)
+    // Author : Anand
+    int[][] dp = new int[16][1 << 16];
+
+    /*
+    This is a traveling salesman problem, so we have to do the full search.
+    We need to memoise for the current city, and cities we have visited.
+    For later, we can use a bit mask.
+    Note:- that we do not need to memoise for k - since it can be inferred from the bit mask.
+ */
+    public int maximumCost(int n, int[][] highways, int k) {
+
+        if (k > n - 1) return -1;
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < graph.length; i++) graph[i] = new ArrayList<>();
+
+        for (int[] h : highways) {
+            graph[h[0]].add(new int[]{h[1], h[2]});
+            graph[h[1]].add(new int[]{h[0], h[2]});
+        }
+
+        int maxi = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, dfs(graph, i, k, 1 << i));
+        }
+
+        return maxi < 0 ? -1 : maxi;
+    }
+
+    private int dfs(List<int[]>[] graph, int start, int k, int mask) {
+        // base case
+
+        if (k == 0) return 0;
+
+        if (dp[start][mask] == 0) {
+
+            dp[start][mask] = Integer.MIN_VALUE;
+            for (int[] j : graph[start]) {
+                if ((mask & (1 << j[0])) == 0) {
+                    int cost = j[1];
+                    dp[start][mask] = Math.max(dp[start][mask], cost + dfs(graph, j[0], k - 1, mask | 1 << j[0]));
+
+                }
+            }
+        }
+
+        return dp[start][mask];
+    }
+
 
 }
 
