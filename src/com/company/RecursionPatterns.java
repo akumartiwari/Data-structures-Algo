34d27227681f92713ac2b47e973c85f42e66db9a
anatiwar <anatiwar@ciena.com>
2022-05-12 13:56:03 +0530

    Word problem recursion

--- src/com/company/RecursionPatterns.java
+++ src/com/company/RecursionPatterns.java
@@ -20,6 +20,23 @@ public class RecursionPatterns {
 //        for (List<Integer> list : ans) {
 //            System.out.println(list.stream().map(Object::toString).collect(Collectors.joining(", ")));
 //        }
+//        String str1 = "iloveicecreamandmango"; // for first test case
+//        String str2 ="ilovesamsungmobile";     // for second test case
+//        int n1 = str1.length();                 // length of first string
+//        int n2 = str2.length();                 // length of second string
+//
+//        // List of strings in dictionary
+//        List <String> dict= Arrays.asList("mobile","samsung","sam","sung",
+//                "man","mango", "icecream","and",
+//                "go","i","love","ice","cream");
+//        System.out.println("First Test:");
+//
+//        // call to the method
+//        wordBreak(n1,dict,str1);
+//        System.out.println("\nSecond Test:");
+//
+//        // call to the method
+//        wordBreak(n2,dict,str2);
 
         System.out.println("jumps=" + minJumps(arr));
     }
@@ -610,5 +627,36 @@ public class RecursionPatterns {
             len = Math.max(len, 1 + recurse(nums, idx + 1, idx, dp));
         }
         return dp[idx][prev_idx + 1] = len;
+    }
+    
+    /*
+    Consider the following dictionary
+    { i, like, sam, sung, samsung, mobile, ice,
+    and, cream, icecream, man, go, mango}
+
+    Input: "ilikesamsungmobile"
+    Output: i like sam sung mobile
+         i like samsung mobile
+     */
+
+    // Prints all possible word breaks of given string
+    static void wordBreak(int n, List<String> dict, String s) {
+        String ans = "";
+        wordBreakUtil(n, s, dict, ans);
+    }
+
+    private static void wordBreakUtil(int n, String s, List<String> dict, String ans) {
+
+        for (int i = 1; i <= n; i++) {
+            String prefix = s.substring(0, i);
+            if (dict.contains(prefix)) {
+                if (i == n) {
+                    ans += prefix;
+                    System.out.println(ans);
+                    return;
+                }
+                wordBreakUtil(n - i, s.substring(i, n), dict, ans + prefix + " ");
+            }
+        }
     }
 }
