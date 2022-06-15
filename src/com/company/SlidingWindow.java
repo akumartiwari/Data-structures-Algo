b92ff997386a1c30a963b1970117d9c5d3d30e65
anatiwar <anatiwar@ciena.com>
2022-06-16 01:51:53 +0530

    contest problems done

--- src/com/company/SlidingWindow.java
+++ src/com/company/SlidingWindow.java
@@ -61,4 +61,57 @@ public class SlidingWindow {
 
         return result;
     }
+
+    // Author: Anand
+    //    Sliding window approach
+    //    The idea is to keep moving i till condition is satisfied. The moment it fails
+    //    start moving j and update result. Then evaluate the number of subarrays possible
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        long prod = 1;
+        int cnt = 0;
+
+        if (k <= 1) return 0;
+
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            prod *= nums[i];
+
+            while (prod >= k) {
+                prod /= nums[j++];
+            }
+
+            cnt += (i - j + 1);
+        }
+        return cnt;
+    }
+
+    /*
+    Input: nums = [2,1,4,3,5], k = 10
+    Output: 6
+    Explanation:
+    The 6 subarrays having scores less than 10 are:
+    - [2] with score 2 * 1 = 2.
+    - [1] with score 1 * 1 = 1.
+    - [4] with score 4 * 1 = 4.
+    - [3] with score 3 * 1 = 3.
+    - [5] with score 5 * 1 = 5.
+    - [2,1] with score (2 + 1) * 2 = 6.
+    Note that subarrays such as [1,4] and [4,3,5] are not considered because their scores are 10 and 36 respectively, while we need scores strictly less than 10.
+     */
+    // Author : Anand
+    public long countSubarrays(int[] nums, long k) {
+        long sum = 0L;
+        long cnt = 0L;
+
+        for (int i = 0, j = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            while ((i - j + 1) * sum >= k) {
+                sum -= nums[j++];
+            }
+            cnt += i - j + 1;
+        }
+
+        return cnt;
+    }
+
 }
