b3d42908c820cad03d1918ab40c710c2860ebbbe
anatiwar <anatiwar@ciena.com>
2022-07-18 12:02:44 +0530

    WIP 18th june contest

--- src/com/company/Bitmask.java
+++ src/com/company/Bitmask.java
@@ -46,5 +46,18 @@ public class Bitmask {
             mask |= 1 << (word.charAt(i) - 'a');
         return mask;
     }
+
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int mask = 1;
+        int bits = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) == 1) bits++;
+            mask <<= 1;
+        }
+        return bits;
+
+        //return Integer.bitCount(n);
+    }
 }
 
