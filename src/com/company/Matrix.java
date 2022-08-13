85c5121b994b9fb7e304c82ae4c7280ee377c1f5
anatiwar <anatiwar@ciena.com>
2022-08-14 02:18:02 +0530

    1 more contest down

--- src/com/company/Matrix.java
+++ src/com/company/Matrix.java
@@ -1,7 +1,6 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Matrix {
@@ -24,58 +23,71 @@ public class Matrix {
     }
 
     /*
-   - Create a matrix of given dimension
-   - Place elements in all 4 directions
- */
-    public int[][] spiralMatrix(int m, int n, ListNode head) {
-        int[][] matrix = new int[m][n];
-        for (int[] mat : matrix) Arrays.fill(mat, -1);
+    - Create an array list
+    - Place elements in all 4 directions diagonally
+    */
+    class Solution {
+        public int[] findDiagonalOrder(int[][] matrix) {
+            int m = matrix.length, n = matrix[0].length;
+            List<Integer> ans = new ArrayList<>();
 
-        if (head == null) return matrix;
-        ListNode temp = head;
+            // R DD D UD
+            int[][] dirs1 = {{0, 1}, {1, -1}, {1, 0}, {-1, 1}};
 
-        List<Integer> nodes = new ArrayList<>();
-        while (temp != null) {
-            nodes.add(temp.val);
-            temp = temp.next;
-        }
+            // R DD R UD
+            int[][] dirs2 = {{0, 1}, {1, -1}, {0, 1}, {-1, 1}};
 
-        // R D L U
-        int[][] dirs = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-        int idx = 0;
-        int x = 0, y = 0;
-        matrix[0][0] = nodes.get(idx++);
-        while (true) {
-            boolean done = false;
-            for (int[] dir : dirs) {
-                int newx = dir[0] + x;
-                int newy = dir[1] + y;
+            int x = 0, y = 0;
 
-                if (idx >= nodes.size()) {
-                    done = true;
-                    break;
-                }
+            int[][] dirs = new int[4][2];
+            ans.add(matrix[x][y]);
+            while (true) {
 
-                while (idx < nodes.size() && safe(newx, newy, matrix)) {
-                    matrix[newx][newy] = nodes.get(idx++);
-                    x = newx;
-                    y = newy;
-                    newx = dir[0] + x;
-                    newy = dir[1] + y;
-                }
+                if (x == 0 && y == n - 1) {
+                    dirs = dirs2;
+                } else dirs = dirs1;
 
-                if (idx >= nodes.size()) {
-                    done = true;
-                    break;
+                for (int[] dir : dirs) {
+
+                    boolean d = false;
+                    int newx = dir[0] + x;
+                    int newy = dir[1] + y;
+
+                    System.out.println(newx + "," + newy);
+                    if (newx == m - 1 && newy == n - 1) {
+                        ans.add(matrix[newx][newy]);
+                        return ans.stream().mapToInt(e -> e).toArray();
+                    }
+
+
+                    while (safe(newx, newy, matrix) && ((dir[0] == 1 && dir[1] == -1) || (dir[0] == -1 && dir[1] == 1))) {
+                        d = true;
+                        ans.add(matrix[newx][newy]);
+
+                        if (newx == 0 && newy == n - 1) {
+                            break;
+                        }
+
+                        x = newx;
+                        y = newy;
+                        newx = dir[0] + x;
+                        newy = dir[1] + y;
+                        System.out.println(newx + "," + newy);
+                    }
+
+
+                    if (!d && safe(newx, newy, matrix)) {
+                        ans.add(matrix[newx][newy]);
+                        x = newx;
+                        y = newy;
+                    }
+
                 }
             }
-
-            if (done) break;
         }
-        return matrix;
-    }
 
-    private boolean safe(int x, int y, int[][] matrix) {
-        return x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length && (matrix[x][y] == -1);
+        private boolean safe(int x, int y, int[][] matrix) {
+            return x >= 0 && x < matrix.length && y >= 0 && y < matrix[0].length;
+        }
     }
 }
