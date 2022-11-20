d521ac8f551c31a8de26807cec19cb6236cae509
anatiwar <anatiwar@ciena.com>
2022-11-20 09:33:36 +0530

    weekly-contest-320

--- src/com/company/EmployeeSolution.java
+++ src/com/company/EmployeeSolution.java
@@ -4674,6 +4674,20 @@ Output: [1,2,2,3,5,6]
         }
     }
 
+
+    public int unequalTriplets(int[] nums) {
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+
 }
 
 
