23c3a8fe7884904e6d47ff579c7d7f83045738d1
anatiwar <anatiwar@ciena.com>
2022-11-28 15:52:44 +0530

    All problems done

--- src/com/company/DP.java
+++ src/com/company/DP.java
@@ -1216,41 +1216,63 @@ public class DP {
     }
 
 
-    //TLE - Can be improved further
+    // TC = O(10*10*N)
+    /*
+       To create a 5 digit palindrome we do not need to care about the middle element.
+       We just need to find subsequence of pattern XY_YX.
+       Calculate number of subsequences of type XY and subsequences of type YX around any given point i and multiply them to find number of subsequences of type XY_YX.
+       Since string only has digits, the time complexity will be 100*n.
+
+    Approach -
+    We will be maintaing the counts of digit in the list cnts
+    Keep 2 arrays pre and suf to store the number of prefixes of type XY and suffixes of type YX. pre[i-1][1][2] means prefixes of type 12 before index i.
+    Similarly suf[i+1][1][2] means suffixes of type 21 after index i
+    Remember given string is made of digits that is 0123456789.
+    That's a total of 10 unique characters
+    Once we have calculated the prefix and suffix lists we just need to multiply pre[i - 1][j][k] with suf[i + 1][j][k] to find number of palindromic subsequences
+     */
     public int countPalindromes(String s) {
-        Map<String, Integer> dp = new HashMap<>();
-        return helper(s, 0, new StringBuilder(), dp);
-    }
+        int n = s.length(), ans = 0;
+        int[][][] prefix = new int[n][10][10], suffix = new int[n][10][10];
 
-    private int helper(String s, int ind, StringBuilder sb, Map<String, Integer> dp) {
-        // base case
-        if (sb.length() == 5) return 1;
-
-        if (ind >= s.length()) return 0;
-        int cnt = 0;
-
-        String key = ind + "-" + sb;
-        if (dp.containsKey(key)) return dp.get(key);
-        if (sb.length() == 0 || sb.length() == 1 || sb.length() == 2) {
-            sb.append(s.charAt(ind));
-            cnt = (cnt + helper(s, ind + 1, sb, dp)) % mod;
-            sb.deleteCharAt(sb.length() - 1);
-        } else if (sb.length() == 3) {
-            if (s.charAt(ind) == sb.charAt(1)) {
-                sb.append(s.charAt(ind));
-                cnt = (cnt + helper(s, ind + 1, sb, dp)) % mod;
-                sb.deleteCharAt(sb.length() - 1);
+        int[] cnts = new int[10];
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - '0';
+            if (i != 0) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        prefix[i][j][k] = prefix[i - 1][j][k];
+                        if (k == c) prefix[i][j][k] += cnts[j];
+                    }
+                }
             }
-        } else if (sb.length() == 4) {
-            if (s.charAt(ind) == sb.charAt(0)) {
-                sb.append(s.charAt(ind));
-                cnt = (cnt + helper(s, ind + 1, sb, dp)) % mod;
-                sb.deleteCharAt(sb.length() - 1);
+            cnts[c]++;
+        }
+
+
+        Arrays.fill(cnts, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int c = s.charAt(i) - '0';
+            if (i != n - 1) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        suffix[i][j][k] = suffix[i + 1][j][k];
+                        if (k == c) suffix[i][j][k] += cnts[j];
+                    }
+                }
+            }
+            cnts[c]++;
+        }
+
+
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    ans = (int) ((ans + (long) prefix[i - 1][j][k] * suffix[i + 1][j][k]) % mod);
+                }
             }
         }
 
-        cnt = (cnt + helper(s, ind + 1, sb, dp)) % mod;
-        dp.put(key, cnt);
-        return cnt;
+        return ans;
     }
 }
