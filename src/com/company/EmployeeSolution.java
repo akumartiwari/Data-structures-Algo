9e68742b6c5fb2526bfaaa5c4a02a53ecff02c03
anatiwar <anatiwar@ciena.com>
2023-02-19 09:49:09 +0530

    3 problems done

--- src/com/company/EmployeeSolution.java
+++ src/com/company/EmployeeSolution.java
@@ -5839,6 +5839,50 @@ Output: [1,2,2,3,5,6]
     }
 
 
+    /*
+    Input: nums1 = [[1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]]
+    Output: [[1,6],[2,3],[3,2],[4,6]]
+    Explanation: The resulting array contains the following:
+    - id = 1, the value of this id is 2 + 4 = 6.
+    - id = 2, the value of this id is 3.
+    - id = 3, the value of this id is 2.
+    - id = 4, the value of this id is 5 + 1 = 6.
+
+     */
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
+
+        for (int i = 0; i < Math.max(nums1.length, nums2.length); i++) {
+            if (i < nums1.length) tm.put(nums1[i][0], tm.getOrDefault(nums1[i][0], 0) + nums1[i][1]);
+            if (i < nums2.length) tm.put(nums2[i][0], tm.getOrDefault(nums2[i][0], 0) + nums2[i][1]);
+        }
+        int[][] array = new int[tm.size()][2];
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : tm.entrySet()) {
+            array[count][0] = entry.getKey();
+            array[count][1] = entry.getValue();
+            count++;
+        }
+
+        return array;
+    }
+
+
+    public int minOperations(int n) {
+        int op = 0;
+        while (n != 0) {
+            if (n == 1) {
+                op++;
+                break;
+            }
+            int num = 1;
+            while (num <= n) num *= 2;
+            num /= 2;
+            n = Math.min(n - num, num * 2 - n);
+            op++;
+        }
+        return op;
+    }
 }
 
 
