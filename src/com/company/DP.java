f8699b078e5069579adf30a7028166e75a767986
anatiwar <anatiwar@ciena.com>
2023-10-08 11:16:54 +0530

    8th oct WIP

--- src/com/company/DP.java
+++ src/com/company/DP.java
@@ -1905,48 +1905,42 @@ It can be proven that there are no more than 3 square-free subsets in the given 
     6
      */
 
-    public int minOperations(String s1, String s2, int x) {
+    class Solution {
+        public int minOperations(String s1, String s2, int x) {
 
-        List<Integer> indices = new ArrayList<>();
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) indices.add(i);
+            List<Integer> indices = new ArrayList<>();
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) indices.add(i);
+            }
+
+            if (indices.size() % 2 != 0) return -1;
+            int mincost = 0;
+
+            List<Integer> clone = new ArrayList<>();
+            clone.addAll(indices);
+            System.out.println("indices=" + Arrays.toString(indices.toArray()));
+
+            return helper(clone, 0, x);
+
         }
 
-        if (indices.size() % 2 != 0) return -1;
-        int mincost = 0;
 
-        List<Integer> clone = new ArrayList<>();
-        clone.addAll(indices);
-        System.out.println("indices=" + Arrays.toString(indices.toArray()));
+        // Take any 2 indexes and get min of two operations
+        private int helper(List<Integer> clone, int ind, int x) {
+            List<Integer> nc = new ArrayList<>();
+            nc.addAll(clone);
 
-
-        while (true) {
-            if (indices.isEmpty()) return mincost;
-            TreeMap<Integer, List<Pair<Integer, Integer>>> tm = new TreeMap<>();
-
-            for (int i = 0; i < indices.size() - 1; i++) {
-                int dist = indices.get(i + 1) - indices.get(i);
-                if (!tm.containsKey(dist)) tm.put(dist, new ArrayList<>());
-                tm.get(dist).add(new Pair<>(indices.get(i + 1), indices.get(i)));
-            }
-
-            if (tm.isEmpty()) return mincost;
-
-            System.out.println(tm);
-            System.out.println(Arrays.toString(indices.toArray()));
-            System.out.println(mincost);
-
-            Map.Entry<Integer, List<Pair<Integer, Integer>>> entry = tm.firstEntry();
-            Set<Integer> s = new HashSet<>();
-            for (Pair<Integer, Integer> point : entry.getValue()) {
-                if (!s.contains(point.getKey()) && !s.contains(point.getValue())) {
-                    mincost += Math.min(x, Math.abs(point.getValue() - point.getKey()));
-                    s.add(point.getKey());
-                    s.add(point.getValue());
-                    indices.remove(new Integer(point.getKey()));
-                    indices.remove(new Integer(point.getValue()));
+            int mc = Integer.MAX_VALUE;
+            int cost = 0;
+            for (int i = 0; i < clone.size() - 1; i++) {
+                for (int j = i + 1; j < clone.size(); j++) {
+                    clone.remove(j);
+                    clone.remove(i);
+                    cost += Math.min(Math.abs(clone.get(j) - clone.get(i)), x) + helper(clone, 0, x);
                 }
             }
+
+            return mc = Math.min(cost, mc);
         }
     }
 
