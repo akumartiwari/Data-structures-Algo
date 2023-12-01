6e2ce01c0288c5b30475d4a98e595304aad8a82f
anatiwar <anatiwar@ciena.com>
2023-12-01 23:19:15 +0530

    Daily LC WIP

--- src/com/company/EmployeeSolution.java
+++ src/com/company/EmployeeSolution.java
@@ -9068,6 +9068,14 @@ Output: [1,2,2,3,5,6]
         return ans;
     }
 
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (String word : word1) sb1.append(word);
+        for (String word : word2) sb2.append(word);
+        return sb1.toString().equals(sb2.toString());
+    }
+
     public boolean checkPowersOfThree(int n) {
         int max = 0;
         while (Math.pow(3, max) <= n) max++;
