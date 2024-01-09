ca353f8b3a86a5910a0c36e351168bab3f337549
Anand tiwari <33821613+akumartiwari@users.noreply.github.com>
2024-01-09 23:25:27 +0530

    Daily lc (#103)
    * Daily LC
    
    * DP based solution
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * 9th dec LC (#101)
    
    * Daily LC WIP
    
    * Daily LC WIP
    
    * Daily LC WIP
    
    * Code Refactored
    
    * Daily LC
    
    * Daily LC
    
    ---------
    
    Co-authored-by: anatiwar <anatiwar@ciena.com>
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC WIP
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    * Daily LC
    
    ---------
    
    Co-authored-by: anatiwar <anatiwar@ciena.com>

--- src/com/company/TreeNode.java
+++ src/com/company/TreeNode.java
@@ -58,6 +58,22 @@ class TreeNode {
         return ans;
     }
 
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        return leaves1.equals(leaves2);
+    }
+
+    private void dfs(TreeNode root, List<Integer> leaves) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) leaves.add(root.val);
+        dfs(root.left, leaves);
+        dfs(root.right, leaves);
+    }
+
     /*
     Use Recursion to traverse subTree of a node and place braces around them
     Traverse the right subtree only of node is present and place braces in the string
