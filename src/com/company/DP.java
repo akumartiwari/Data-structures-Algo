3fc2fca83a8c8187d691cd1e750491c822b45f59
anatiwar <anatiwar@ciena.com>
2025-08-31 19:08:47 +0530

    LC contest 31st aug wip

--- src/com/company/DP.java
+++ src/com/company/DP.java
@@ -2224,14 +2224,10 @@ It can be proven that there are no more than 3 square-free subsets in the given 
         }
     }
 
-    //Solve using DP pattern of repeating same number multiple times in an array
     class SolutionMinDifference {
-        public int[] minDifference(int n, int k) {
-            System.out.println(printDivisors(100000));
-            return new int[3];
-        }
+        Set<List<Integer>> ans = new HashSet<>();
 
-        public ArrayList<Integer> printDivisors(int n) {
+        public int[] printDivisors(int n) {
             // Note that this loop runs till square root
 
             ArrayList<Integer> list = new ArrayList<>();
@@ -2248,7 +2244,50 @@ It can be proven that there are no more than 3 square-free subsets in the given 
                 }
             }
 
-            return list;
+            return list.stream().mapToInt(x -> x).toArray();
+        }
+
+        public int[] minDifference(int n, int k) {
+            int[] candidates = printDivisors(n);
+            Arrays.sort(candidates);
+            List<Integer> ds = new ArrayList<>();
+            bfs(candidates, n, 0, 1, ds, k);
+            List<Integer> result = new ArrayList<>();
+
+            int minDiff = Integer.MAX_VALUE;
+            for (List<Integer> res : ans) {
+
+                int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+                for (int e : res) {
+                    max = Math.max(max, e);
+                    min = Math.min(min, e);
+                }
+
+                if (max - min < minDiff) {
+                    minDiff = max - min;
+                    result = res;
+                }
+            }
+
+            return result.stream().mapToInt(x -> x).toArray();
+        }
+
+        private void bfs(int[] arr, int n, int idx, int prod, List<Integer> ds, int k) {
+
+            if (ds.size() > k || prod > n) return;
+            // base case
+            if (prod == n && ds.size() == k) {
+                ans.add(new ArrayList<>(ds));
+            } else {
+                for (int i = idx; i < arr.length; i++) {
+                    // take
+                    prod *= arr[i];
+                    ds.add(arr[i]);
+                    bfs(arr, n, i, prod, ds, k);
+                    prod /= arr[i];
+                    ds.remove(new Integer(arr[i])); // remove last element
+                }
+            }
         }
     }
 }
